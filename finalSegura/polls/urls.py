8ce96e9d7efa8a7d412603536e77a5abfa4ef112
erickdbn <erale1997@hotmail.com>
2021-06-26 15:14:29 -0500

    - Se agregan nuevas rutas en urls.

--- finalSegura/polls/urls.py
+++ finalSegura/polls/urls.py
@@ -7,10 +7,11 @@ urlpatterns = [
     path('', views.feed, name='feed'),
     path('registro/', views.registro, name='registro'),
     path('login/', LoginView.as_view(template_name='polls/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='polls/logout.html'), name='logout'),
     path('token/',views.token,name='token'),
     path('Ingresar/', views.ingresar, name='ingresar'),
     path('credenciales/', views.credenciales, name='credenciales'),
     path('registrar_credencial', views.registrar_credencial, name='registrar_credencial'),
     path('listar', views.credenciales_list, name='credenciales_list'),
+    path('logout', views.logout, name='logout'),
+    path('registro/info.html', views.info, name='info'),
     ]
