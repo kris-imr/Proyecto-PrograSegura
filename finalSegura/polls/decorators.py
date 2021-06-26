cc78c362b9bb9784571bba1af13997834be61407
erickdbn <erale1997@hotmail.com>
2021-06-26 15:11:37 -0500

    - Se modifica el decorador.

--- finalSegura/polls/decorators.py
+++ finalSegura/polls/decorators.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
 def login_requerido2(vista):
     def interna(request, *args, **kwargs):
-        if not request.user.username:
-            return redirect ('/login')
+        logueado = request.user.is_staff
+        if logueado == False:
+            request.session.flush()
+            return redirect('/login')
         return vista(request, *args, **kwargs)
-        
     return interna
 
