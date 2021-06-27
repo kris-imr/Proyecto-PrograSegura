7285250088bdb67f63ef927e556cc1a3f847aebd
LeoDeviluke <leo13droid@gmail.com>
2021-06-26 21:02:53 -0500

    -se utilizaron expresiones regulares para las políticas de contraseña segura

--- finalSegura/polls/forms.py
+++ finalSegura/polls/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from polls.models import Perfil
 
 class UserForm(forms.ModelForm):
-    password=forms.CharField(widget=forms.PasswordInput)
-    confirmar_password=forms.CharField(widget=forms.PasswordInput)
+    password=forms.CharField(min_length=12, validators=[RegexValidator(regex=r'^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])[A-Za-z\d$@$!%?&]', message='La contraseña debe tener lo siguiente: minúsculas, MAYÚSCULAS, números, caractere especiales ($@$!%*?&) y ser 12')], widget=forms.PasswordInput)
+    confirmar_password=forms.CharField(min_length=12, validators=[RegexValidator(regex=r'^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[$@$!%?&])[A-Za-z\d$@$!%?&]')], widget=forms.PasswordInput)
     class Meta:
         model = Perfil
         fields = ['username', 'first_name', 'last_name', 'email', 'Token', 'chatID', 'password', 'confirmar_password']
