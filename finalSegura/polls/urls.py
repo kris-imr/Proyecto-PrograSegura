5f0a66cbd02bf07acfb101653d251bc64b53742d
kris-imr <kristenirazu@gmail.com>
2021-07-02 05:45:29 -0500

    Se edito urls.py

--- finalSegura/polls/urls.py
+++ finalSegura/polls/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.feed, name='feed'),
     path('registro/', views.registro, name='registro'),
     path('login/', LoginView.as_view(template_name='polls/login.html'), name='login'),
+    #path('logout/', LogoutView.as_view(template_name='polls/logout.html'), name='logout'),
     path('token/',views.token,name='token'),
     path('Ingresar/', views.ingresar, name='ingresar'),
     path('credenciales/', views.credenciales, name='credenciales'),
@@ -14,4 +15,6 @@ urlpatterns = [
     path('listar', views.credenciales_list, name='credenciales_list'),
     path('logout', views.logout, name='logout'),
     path('registro/info.html', views.info, name='info'),
-    ]
+    path('Ingresar/fail.html', views.fail, name='fail.html'),
+    path('usuarioEdit.html', views.edit, name='usuarioEdit.html')
+    ]]
